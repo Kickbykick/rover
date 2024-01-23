21b38c120d9327c8b0d4b739a80d8faacff1db04
Kickbykick <kiibatitoo@gmail.com>
2024-01-23 13:49:33 -0600

    adding 14 images

--- src/config/data.ts
+++ src/config/data.ts
@@ -1,1 +1,16 @@
-export const imageName=["Rover-1.png", "Rover-2.png"];
+export const imageName=[
+    "Rover Designs-01.png",
+    "Rover Designs-02.png",
+    "Rover Designs-03.png",
+    "Rover Designs-04.png",
+    "Rover Designs-05.png",
+    "Rover Designs-06.png",
+    "Rover Designs-07.png",
+    "Rover Designs-08.png",
+    "Rover Designs-09.png",
+    "Rover Designs-10.png",
+    "Rover Designs-11.png",
+    "Rover Designs-12.png",
+    "Rover Designs-13.png",
+    "Rover Designs-14.png"
+];
